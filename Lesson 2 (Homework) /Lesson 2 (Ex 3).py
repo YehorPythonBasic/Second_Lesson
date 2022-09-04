2b0352b88070815751a52b27dd08ae662d8db7d0
Yehor Sivak <skatchbox@gmail.com>
2022-09-04 14:42:24 +0300

    Lesson 2 Homework Exercise 3

---
+++ Lesson 2 (Homework) /Lesson 2 (Ex 3).py
@@ -0,0 +1,9 @@
+slash = '\\'
+
+print(slash + 'a' + '\t' + 'Bell (alert)' +
+      '\n' + slash + 'b' + '\t' + 'Backspace' +
+      '\n' + slash + 'n' + '\t' + 'New line' +
+      '\n' + slash + 't' + '\t' + 'Horizontal tab' +
+      '\n' + (slash * 2) + '\t' + 'Backslash \\' +
+      '\n' + slash + '"' + '\t' + 'Double quotation mark "' +
+      '\n' + slash + "'" + '\t' + "Single quotation mark '")
