6227f710087e4eb885b0839586785a2dc804528a
Yehor Sivak <skatchbox@gmail.com>
2022-09-18 21:27:42 +0300

    Homework Exercise 1 (update for result)

--- Lesson 4 (Homework)/Lesson 4 (Ex 1).py
+++ Lesson 4 (Homework)/Lesson 4 (Ex 1).py
@@ -3,6 +3,7 @@ a = float(input('Введите первое число: '))
 operator = input('Введите оператор: ')
 b = float(input('Введите второе число: '))
 podschet = 'zatichka'
+chisla_posle_tochki = 'zatichka'
 
 # Решение в зависимости от введенного оператора
 if operator == '+':
@@ -16,7 +17,15 @@ elif operator == '/' and b != 0:
 elif operator == '**':
     podschet = a ** b
 
-# Вывод решения и проверка на корректный ввод
+# Убераем числа после точки, если их нет, так как тип float и он будет всегда дробным
+if podschet != 'zatichka':
+    chisla_posle_tochki = (podschet / 1) - (podschet // 1)
+if chisla_posle_tochki != 'zatichka' and chisla_posle_tochki > 0:
+    podschet = float(podschet)
+if chisla_posle_tochki != 'zatichka' and chisla_posle_tochki == 0:
+    podschet = int(podschet)
+
+# Вывод решения и проверка на корректный ввод оператора и деления на 0
 if podschet != 'zatichka':
     print(f'Решение:{podschet}')
 else:
