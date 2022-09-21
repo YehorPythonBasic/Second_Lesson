782e932f72b3c06975b80949bf92890eb57eb65d
Yehor Sivak <skatchbox@gmail.com>
2022-09-21 23:40:58 +0300

    Homework Exercise 1B

---
+++ Lesson 5 (Homework)/Lesson 5 (Ex 1B).py
@@ -0,0 +1,9 @@
+height = int(input('Введите высоту фигуры: '))
+
+for h in range(height):
+    for i in range(h, height - 1):
+        print(' ', end=' ')
+    for w in range(2 * h + 1):
+        if w == 0 or w != 0:
+            print('*', end=' ')
+    print()
