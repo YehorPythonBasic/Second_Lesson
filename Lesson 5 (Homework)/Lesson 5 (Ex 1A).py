473f6aaec50267c3d8fec48e43491f1632b7cf36
Yehor Sivak <skatchbox@gmail.com>
2022-09-21 23:52:46 +0300

    Homework Exercise 1A

--- Lesson 5 (Homework)/Lesson 5 (Ex 1A).py
+++ Lesson 5 (Homework)/Lesson 5 (Ex 1A).py
@@ -2,7 +2,9 @@ height = int(input('Введите высоту фигуры: '))
 
 for h in range(height):
     for w in range(2 * height - 1):
-        if w == height - 1 - h or w == height - 1 + h or h == height - 1:
+        if w == height - 1 - h or \
+                w == height - 1 + h or \
+                h == height - 1:
             print('*', end=' ')
         else:
             print(' ', end=' ')
