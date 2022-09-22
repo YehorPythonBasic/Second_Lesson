e92135c5358dbce1aca987d3aa448924ba9b21e0
Yehor Sivak <skatchbox@gmail.com>
2022-09-22 14:30:58 +0300

    Homework Exercise 1С

---
+++ Lesson 5 (Homework)/Lesson 5 (Ex 1C).py
@@ -0,0 +1,19 @@
+# Ввод высоты
+height = int(input('Введите высоту фигуры: '))
+
+# Верхний треугольник
+for h in range(height):
+    for i in range(h, height - 1):
+        print(' ', end=' ')
+    for w in range(2 * h + 1):
+        if w == 0 or w != 0:
+            print('*', end=' ')
+    print()
+    # Нижний треугольник
+for h in range(height - 1):
+    for w in range(2 * height):
+        if w - height + 2 == height - 1 - h or w + height - 1 == height + h:
+            print('*', end=' ')
+        else:
+            print(' ', end=' ')
+    print()
