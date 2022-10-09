163a5699a485778287270d508bc9d3dcee965606
Yehor Sivak <skatchbox@gmail.com>
2022-10-09 21:38:09 +0300

    Homework Exercise 2

---
+++ Lesson 9 (Homework)/Lesson 9 (Ex 2)/Check (Ex 2).py
@@ -0,0 +1,8 @@
+# Подключение модуля с функцией
+from function_find_a import find_first_a
+
+# Использование функции (find_first_a) из модуля (function_find_a)
+p = find_first_a(['apple', 'top', 'check', 'able', 'air', 'test'])
+
+# Вывод
+print(p)
