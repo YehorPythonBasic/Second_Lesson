0c0842c5796d4e564d28c3c75992bcf91047ce77
Yehor Sivak <skatchbox@gmail.com>
2022-10-09 20:57:39 +0300

    Homework Exercise 1

---
+++ Lesson 9 (Homework)/Lesson 9 (Ex 1)/Check (Ex 1).py
@@ -0,0 +1,8 @@
+# Подключение модуля с функцией
+from function_new_list import rever
+
+# Использование функции (rever) из модуля (function_new_list)
+p = rever(['test', 'hello', 'table', 'car', 'banana', 'apple'])
+
+# Вывод
+print(p)
