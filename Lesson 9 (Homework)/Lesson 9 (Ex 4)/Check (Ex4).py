571ce27dd060e150e83da1cb3867a71ea6ec8c3d
Yehor Sivak <skatchbox@gmail.com>
2022-10-10 12:18:16 +0300

    Homework Exercise 4

---
+++ Lesson 9 (Homework)/Lesson 9 (Ex 4)/Check (Ex4).py
@@ -0,0 +1,8 @@
+# Подключение модуля с функцией
+from function_find_string import find_strings
+
+# Использование функции (find_strings) из модуля (function_find_string)
+my_list = find_strings([1, 2, 3, "test", 33, 'apple', 1.5, 'join'])
+
+# Вывод
+print(my_list)
