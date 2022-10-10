d02bbd892f4c6517c82f638c52855d8b3b59ed21
Yehor Sivak <skatchbox@gmail.com>
2022-10-10 19:35:53 +0300

    Homework Exercise 5

---
+++ Lesson 9 (Homework)/Lesson 9 (Ex 5)/Check (Ex 5).py
@@ -0,0 +1,8 @@
+# Подключение модуля с функцией
+from function_only_one import one_symbol_string
+
+# Использование функции (one_symbol_string) из модуля (function_only_one)
+my_str = one_symbol_string('aaa b ccc eee f g kkk h')
+
+# Вывод
+print(my_str)
