5085ebf39377897777e925e2795f16fbebffc5c9
Yehor Sivak <skatchbox@gmail.com>
2022-10-10 12:05:31 +0300

    Homework Exercise 3

--- Lesson 9 (Homework)/Lesson 9 (Ex 3)/Check (Ex 3).py
+++ Lesson 9 (Homework)/Lesson 9 (Ex 3)/Check (Ex 3).py
@@ -2,7 +2,7 @@
 from function_any_a import any_a
 
 # Использование функции (any_a) из модуля (function_any_a)
-p = any_a(['tablet', 'test', 'pad', 'pop', 'table'])
+my_list = any_a(['tablet', 'test', 'pad', 'pop', 'table'])
 
 # Вывод
-print(p)
+print(my_list)
