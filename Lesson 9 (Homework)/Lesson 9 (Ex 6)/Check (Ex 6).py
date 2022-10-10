2e0452e333beb297835d87ffbf6f4d581872300b
Yehor Sivak <skatchbox@gmail.com>
2022-10-10 20:25:31 +0300

    Homework Exercise 6

---
+++ Lesson 9 (Homework)/Lesson 9 (Ex 6)/Check (Ex 6).py
@@ -0,0 +1,8 @@
+# Подключение модуля с функцией
+from function_symbols_strings import symbols_in_strings
+
+# Использование функции (symbols_in_strings) из модуля (function_symbols_strings)
+p = symbols_in_strings(str_one='test1234', str_two='apple23')
+
+# Вывод
+print(p)
