74b3f610f246120a74aa75fa95c1f917d5a75b60
Yehor Sivak <skatchbox@gmail.com>
2022-10-10 11:58:00 +0300

    Homework Exercise 3

---
+++ Lesson 9 (Homework)/Lesson 9 (Ex 3)/Check (Ex 3).py
@@ -0,0 +1,8 @@
+# Подключение модуля с функцией
+from function_any_a import any_a
+
+# Использование функции (any_a) из модуля (function_any_a)
+p = any_a(['tablet', 'test', 'pad', 'pop', 'table'])
+
+# Вывод
+print(p)
