eb304c878d4269af8ef94302728e05c1c6e34e50
Yehor Sivak <skatchbox@gmail.com>
2022-10-10 12:05:53 +0300

    Homework Exercise 2

--- Lesson 9 (Homework)/Lesson 9 (Ex 2)/Check (Ex 2).py
+++ Lesson 9 (Homework)/Lesson 9 (Ex 2)/Check (Ex 2).py
@@ -2,7 +2,7 @@
 from function_find_a import find_first_a
 
 # Использование функции (find_first_a) из модуля (function_find_a)
-p = find_first_a(['apple', 'top', 'check', 'able', 'air', 'test'])
+my_list = find_first_a(['apple', 'top', 'check', 'able', 'air', 'test'])
 
 # Вывод
 print(p)
