aba86148f00c22fdacc5affb0753f52487f72196
Yehor Sivak <skatchbox@gmail.com>
2022-10-10 21:13:09 +0300

    Homework Exercise 7

---
+++ Lesson 9 (Homework)/Lesson 9 (Ex 7)/Check (Ex 7).py
@@ -0,0 +1,8 @@
+# Подключение модуля с функцией
+from function_one_in_strings import one_symbol_in_strings
+
+# Использование функции (one_symbol_in_strings) из модуля (function_one_in_strings)
+p = one_symbol_in_strings(str_one='aabsssrrr122233', str_two='appppr123b')
+
+# Вывод
+print(p)
